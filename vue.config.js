3640059749c725a6fd4560f85600d1c1692c7491
Said Sarabia Saavedra <ssarabia@sales-hub.com>
2022-07-02 00:44:10 -0700

    added dist build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "kyasa-landingpage" : "/"
+}
 module.exports = defineConfig({
   transpileDependencies: true
 })
